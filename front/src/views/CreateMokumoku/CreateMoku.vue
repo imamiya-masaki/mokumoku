682abe92c07b85ace152af5cbc20f483cb1d88e7
imamiya-masaki <imachan567@gmail.com>
2020-05-05 08:06:57 +0900

    git連携の方法を考える

---
+++ front/src/views/CreateMokumoku/CreateMoku.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="CreateMoku">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateMoku',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted: function () {
+  },
+  computed: {
+  },
+  method: {
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios'
+import Vue from 'vue'
+const URL_BASE = '/api/search/'
 
 export default {
   name: 'Home',
   components: {
+  },
+  data () {
+    return {
+      url: 'https://github.com/imamiya-masaki/mokumoku/commits/',
+      search_list: []
+    }
+  },
+  mounted: function () {
+  },
+  computed: {
+  },
+  methods: {
+    get_ajax (url, name) {
+      return axios.get(URL_BASE + url)
+        .then((res) => {
+          Vue.set(this, name, res.data)
+          this.$emit('GET_AJAX_COMPLETE')
+        })
+    }
   }
 }
 </script>
+
+<style lang="scss">
+</style>
